49563afc7a174c411583e87f1f97640be3507620
Mohammad Ramiz <ramizanas6@gmail.com>
2025-07-24 06:46:20 +0530

    fix

--- sender.py
+++ sender.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, Request, HTTPException, Header
 from pydantic import BaseModel
 import firebase_admin
 from firebase_admin import credentials, firestore
